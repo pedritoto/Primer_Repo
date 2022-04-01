45c8eb9dc23b3c935cce9f83f2c60f3f10679042
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-03-31 21:16:06 -0700

    magic

--- MyApp.py
+++ MyApp.py
@@ -4,3 +4,11 @@ st.button('Click aquí')
 st.balloons()
 st.markdown("$ \int_0^5 $")
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+fig
