fd800c5c5a4a476319c31ac32deeedbaae6bae1f
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-03-31 23:00:47 -0700

    d

--- MyApp.py
+++ MyApp.py
@@ -24,8 +24,19 @@ import time
 
 my_bar = st.progress(0)
 
-for percent_complete in range(100):
-     time.sleep(0.1)
-     my_bar.progress(percent_complete + 1)
+#for percent_complete in range(100):
+#     time.sleep(0.1)
+#     my_bar.progress(percent_complete + 1)#
 
-st.snow()     
+#st.snow()     
+with st.form("my_form"):
+    st.write("Inside the form")
+    slider_val = st.slider("Form slider")
+    checkbox_val = st.checkbox("Form checkbox")
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("slider", slider_val, "checkbox", checkbox_val)
+
+st.write("Outside the form")
