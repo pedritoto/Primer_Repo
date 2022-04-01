30cf45bcaf24b4fc8d385fc3c36ec4857ff8ce8b
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-03-31 22:49:53 -0700

    d

--- MyApp.py
+++ MyApp.py
@@ -17,5 +17,5 @@ st.image('1tqn-min.png', caption='Sunrise by the mountains')
 import numpy as np
 x = np.linspace(-10,10,100,dtype=float)
 y = np.sin(x)
-fig = plt.plot(x,y).show()
-fig
+plt.plot(x,y)
+plt.show()
