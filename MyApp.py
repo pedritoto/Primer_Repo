28ffabd7da1d8d08928e5dace7f185a0a240ec3f
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-03-31 23:33:27 -0700

    d

--- MyApp.py
+++ MyApp.py
@@ -41,8 +41,15 @@ with st.form("my_form"):
 
 st.write("Outside the form")
 
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
 
 add_selectbox = st.sidebar.selectbox(
     "How would you like to be contacted?",
     ("Email", "Home phone", "Mobile phone")
 )
+
