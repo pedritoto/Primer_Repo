d9e330dbedd4f46d1d4689887df8fce4a14d3706
pedritoto <84814111+pedritoto@users.noreply.github.com>
2022-03-31 23:37:15 -0700

    d

--- MyApp.py
+++ MyApp.py
@@ -29,17 +29,9 @@ my_bar = st.progress(0)
 #     my_bar.progress(percent_complete + 1)#
 
 #st.snow()     
-with st.form("my_form"):
-    st.write("Inside the form")
-    slider_val = st.slider("Form slider")
-    checkbox_val = st.checkbox("Form checkbox")
 
-    # Every form must have a submit button.
-    submitted = st.form_submit_button("Submit")
-    if submitted:
-        st.write("slider", slider_val, "checkbox", checkbox_val)
 
-st.write("Outside the form")
+
 
 # Using "with" notation
 with st.sidebar:
@@ -50,6 +42,15 @@ with st.sidebar:
     
 if add_radio=='Standard (5-15 days)':
         st.latex("\int_0^7")
+        with st.form("my_form"):
+          st.write("Inside the form")
+          slider_val = st.slider("Form slider")
+          checkbox_val = st.checkbox("Form checkbox")
+
+         # Every form must have a submit button.
+          submitted = st.form_submit_button("Submit")
+           if submitted:
+              st.write("slider", slider_val, "checkbox", checkbox_val)  
 add_selectbox = st.sidebar.selectbox(
     "How would you like to be contacted?",
     ("Email", "Home phone", "Mobile phone")
